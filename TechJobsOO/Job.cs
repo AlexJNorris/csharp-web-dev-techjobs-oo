5023ad93aef0a35f2d6c3d58ae4cf4893e09376b
AlexJNorris <a.norris97@icloud.com>
2020-10-27 21:08:31 -0500

    just fucking work

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+
 namespace TechJobsOO
 {
     public class Job
@@ -11,9 +13,43 @@ namespace TechJobsOO
         public Location EmployerLocation { get; set; }
         public PositionType JobType { get; set; }
         public CoreCompetency JobCoreCompetency { get; set; }
+        private string[] JobItems = {"Id: ", "Name: ", "Employer: ", "Location: ", "Position Type: ", "Core Competency: " };
 
-        // TODO: Add the two necessary constructors.
+        public Job()
+        {
+            Id = nextId;
+            nextId++;
+        }
 
-        // TODO: Generate Equals() and GetHashCode() methods.
+        public Job(string v, Employer emp, Location loc, PositionType posT, CoreCompetency CorC) : this()
+        {
+            Name = v;
+            EmployerName = emp;
+            EmployerLocation = loc;
+            JobType = posT;
+            JobCoreCompetency = CorC;
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+        public override string ToString()
+        {
+            string str;
+            str = '\n' + JobItems[0] + this.Id.ToString() + '\n' + JobItems[1] + this.Name + '\n'
+                + JobItems[2] + this.EmployerName.Value + '\n' + JobItems[3] + this.EmployerLocation.Value + '\n'
+                + JobItems[4] + this.JobType.Value + '\n' + JobItems[5] + this.JobCoreCompetency.ToString() + '\n';
+
+
+            return str;
+        }
     }
 }
---
+++ TechJobsTests/JobTests.cs
@@ -0,0 +1,59 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
+
+namespace TechJobsTests
+{
+    [TestClass]
+    public class JobTests
+    {
+        Job job1;
+        Job job2;
+        Job job3;
+        Job job4;
+        Job empty;
+        string testString1;
+        string testString2;
+
+       [TestInitialize]
+        public void CreateCarObject()
+        {
+            job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+            job3 = new Job("Ice cream tester", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Tasting ability"));
+            job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            empty = new Job("",new Employer(""),new Location(""), new PositionType(""), new CoreCompetency(""));
+            testString1 = '\n' + "Id: 20\n" + "Name: \n" + "Employer: \n" + "Location: \n" + "Position Type: \n" + "Core Competency: \n";
+            testString2 = '\n' + "Id: 22\n" + "Name: Web Developer\n" + "Employer: LaunchCode\n" + "Location: St. Louis\n" + "Position Type: Front-end developer\n" + "Core Competency: JavaScript\n";
+        }
+
+        [TestMethod]
+        public void TestSettingJobId()
+        {
+            Assert.IsFalse((job1.Id == job2.Id));
+        }
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Assert.AreEqual("Product tester", job1.Name);
+            Assert.AreEqual("ACME", job1.EmployerName.Value);
+            Assert.AreEqual("Desert", job1.EmployerLocation.Value);
+            Assert.AreEqual("Quality control", job1.JobType.Value);
+            Assert.AreEqual("Persistence", job1.JobCoreCompetency.ToString());
+        }
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Assert.IsFalse(job1.Equals(job4));
+        }
+        [TestMethod]
+        public void TestForBlankLines()
+        {
+            Assert.AreEqual(testString1, empty.ToString());
+        }
+        [TestMethod]
+        public void TestForCorrectFormat()
+        {
+            Assert.AreEqual(testString2, job2.ToString());
+        }
+    }
+}
